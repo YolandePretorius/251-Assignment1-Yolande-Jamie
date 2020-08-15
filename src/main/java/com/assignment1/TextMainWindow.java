1feb10218a3118b65b361430c3c47c4ecc9d4c1c
Yolande <yolande.y.kok@gmail.com>
2020-08-16 00:28:13 +1200

    Signed-off-by: Yolande <yolande.y.kok@gmail.com>

--- src/main/java/com/assignment1/TextMainWindow.java
+++ src/main/java/com/assignment1/TextMainWindow.java
@@ -1,13 +1,11 @@
 package com.assignment1;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridLayout;
-import javax.swing.JToolBar;
 import javax.swing.BoxLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -33,10 +31,18 @@ import javax.swing.JSpinner;
 import javax.swing.JSeparator;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JEditorPane;
+import java.awt.TextField;
+import javax.swing.JTextArea;
+import java.awt.TextArea;
 
 public class TextMainWindow extends JFrame {
 
 	private JPanel contentPane;
+	/**
+	 * @wbp.nonvisual location=185,-16
+	 */
+	private final JEditorPane editorPane = new JEditorPane();
 
 	/**
 	 * Launch the application.
@@ -58,6 +64,7 @@ public class TextMainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public TextMainWindow() {
+		setTitle("Text Pad");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
@@ -96,11 +103,44 @@ public class TextMainWindow extends JFrame {
 		});
 		mnNewMenu.add(mntmNewMenuItem_4);
 		
+		JMenuItem mntmNewMenuItem_5 = new JMenu("Search");
+		menuBar.add(mntmNewMenuItem_5);
+		
+		JMenu mnNewMenu_3 = new JMenu("View");
+		menuBar.add(mnNewMenu_3);
+		
 		JMenu mnNewMenu_1 = new JMenu("Edit");
 		menuBar.add(mnNewMenu_1);
+		
+		JSeparator separator_3 = new JSeparator();
+		mnNewMenu_1.add(separator_3);
+		
+		JMenuItem mntmNewMenuItem_6 = new JMenuItem("Cut");
+		mnNewMenu_1.add(mntmNewMenuItem_6);
+		
+		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Copy");
+		mnNewMenu_1.add(mntmNewMenuItem_7);
+		
+		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Paste");
+		mnNewMenu_1.add(mntmNewMenuItem_8);
+		
+		JSeparator separator_4 = new JSeparator();
+		mnNewMenu_1.add(separator_4);
+		
+		JMenuItem mntmNewMenuItem_9 = new JMenuItem("Time/Date");
+		mnNewMenu_1.add(mntmNewMenuItem_9);
+		
+		JMenu mnNewMenu_2 = new JMenu("Help");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenuItem mntmNewMenuItem_10 = new JMenuItem("About Text Pad");
+		mnNewMenu_2.add(mntmNewMenuItem_10);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		TextArea textArea = new TextArea();
+		contentPane.add(textArea);
 	}
 }
